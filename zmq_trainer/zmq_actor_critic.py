980aa7fc256bcd14a1b9b3c60482455ce043e8b6
jxwuyi <jxwuyi@gmail.com>
2017-09-28 14:03:10 -0700

    update

--- zmq_trainer/zmq_actor_critic.py
+++ zmq_trainer/zmq_actor_critic.py
@@ -16,7 +16,7 @@ from zmq_trainer.zmqsimulator import SimulatorProcess, SimulatorMaster, ensure_p
 flag_max_lrate = 5e-3
 flag_min_lrate = 1e-5
 flag_max_kl_diff = 1e-2
-flag_min_kl_diff = 1e-5
+flag_min_kl_diff = 1e-4
 flag_lrate_coef = 1.5
 
 class ZMQA3CTrainer(AgentTrainer):
