009c82a406e338ffa99c36692c38baff0be2deb5
jxwuyi <jxwuyi@gmail.com>
2018-02-01 17:35:22 -0800

    add icml script

--- icml_scripts/script_train_a3c.sh
+++ icml_scripts/script_train_a3c.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0,1 python3 zmq_train.py --seed 0 --env-set train \
-    --n-house 20 --n-proc 20 --batch-size 64 --t-max 5 --max-episode-len 100 \
-    --hardness 0.9 --reward-type delta --multi-target --use-target-gating \
+CUDA_VISIBLE_DEVICES=1,2 python3 zmq_train.py --seed 0 --env-set small \
+    --n-house 20 --n-proc 80 --batch-size 64 --t-max 25 --max-episode-len 100 \
+    --hardness 0.95 --reward-type delta --success-measure see \
+    --multi-target --use-target-gating \
     --segmentation-input color --depth-input --resolution normal \
-    --max-iters 600000 \
+    --render-gpu 0,1 --max-iters 100000 \
     --algo a3c --lrate 0.001 --weight-decay 0.00001 --gamma 0.95 --batch-norm \
-    --entropy-penalty 0.1 --q-loss-coef 1.0 --grad-clip 0.5 \
+    --entropy-penalty 0.1 --q-loss-coef 1.0 --grad-clip 1.0 \
     --rnn-units 256 --rnn-layers 1 --rnn-cell lstm \
-    --report-rate 20 --save-rate 1000 --eval-rate 100000 \
-    --save-dir ./_model_/zmq_a3c/train_multitarget/medium_color/bn_bc64_tmax5_gate \
-    --log-dir ./log/zmq_a3c/train_multitarget/medium_color/bn_bc64_tmax5_gate 
+    --report-rate 20 --save-rate 1000 --eval-rate 200000 \
+    --save-dir ./_model_/icml/small/hard_seg/bc64_gate_tmax25 \
+    --log-dir ./log/icml/small/hard_seg/bc64_gate_tmax25
 
