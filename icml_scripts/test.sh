35817723fda3e93162066b35b0a6a452f4e382d6
Yi WU <jxwuyi@gmail.com>
2018-02-04 16:47:55 -0800

    add test scripts

--- icml_scripts/test.sh
+++ icml_scripts/test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=1,2 python3 zmq_train.py --seed 0 --env-set small \
-    --n-house 3 --n-proc 5 --batch-size 5 --t-max 25 --max-episode-len 100 \
+CUDA_VISIBLE_DEVICES=0,1,2 python3 zmq_train.py --seed 0 --env-set train \
+    --n-house 3 --n-proc 3 --batch-size 3 --t-max 2 --max-episode-len 5 \
     --hardness 0.95 --reward-type delta --success-measure see \
-    --multi-target --use-target-gating \
+    --multi-target --use-target-gating --include-object-target \
     --segmentation-input color --depth-input --resolution normal \
-    --render-gpu 0,1 --max-iters 100000 \
+    --render-gpu 0,1,2 --max-iters 100000 \
     --algo a3c --lrate 0.001 --weight-decay 0.00001 --gamma 0.95 --batch-norm \
     --entropy-penalty 0.1 --q-loss-coef 1.0 --grad-clip 1.0 \
     --rnn-units 256 --rnn-layers 1 --rnn-cell lstm \
