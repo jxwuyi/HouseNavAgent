5f597d41ac23f18978c27ecbed3e5747866ffd7d
Yi WU <jxwuyi@gmail.com>
2018-02-05 14:19:57 -0800

    update script

---
+++ icml_scripts/script_ddpg_seg_gate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CUDA_VISIBLE_DEVICES=0,1 python3 train.py --seed 0 --env-set small --render-gpu 1 \
+    --algo ddpg_joint --update-freq 5 --max-episode-len 100 \
+    --house -20 --reward-type delta --success-measure see \
+    --multi-target --use-target-gating --use-action-gating \
+    --lrate 0.0001 --gamma 0.95 \
+    --batch-size 128 --hardness 0.95 --replay-buffer-size 800000 \
+    --weight-decay 0.00001 --critic-penalty 0.0001 --entropy-penalty 0.01 \
+    --batch-norm --no-debug \
+    --noise-scheduler high --q-loss-coef 100 \
+    --segmentation-input color --depth-input --resolution normal --history-frame-len 4 \
+    --save-dir ./_model_/icml/small/hard_seg/ddpg_gate_hist_4 \
+    --log-dir ./log/icml/small/hard_seg/ddpg_gate_hist_4
+
--- icml_scripts/script_train_a3c.sh
+++ icml_scripts/script_train_a3c.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=1,2 python3 zmq_train.py --seed 0 --env-set small \
-    --n-house 20 --n-proc 80 --batch-size 64 --t-max 25 --max-episode-len 100 \
+CUDA_VISIBLE_DEVICES=0,1,2 python3 zmq_train.py --seed 0 --env-set small \
+    --n-house 20 --n-proc 90 --batch-size 64 --t-max 20 --max-episode-len 100 \
     --hardness 0.95 --reward-type delta --success-measure see \
     --multi-target --use-target-gating \
     --segmentation-input color --depth-input --resolution normal \
@@ -10,6 +10,6 @@ CUDA_VISIBLE_DEVICES=1,2 python3 zmq_train.py --seed 0 --env-set small \
     --entropy-penalty 0.1 --q-loss-coef 1.0 --grad-clip 1.0 \
     --rnn-units 256 --rnn-layers 1 --rnn-cell lstm \
     --report-rate 20 --save-rate 1000 --eval-rate 200000 \
-    --save-dir ./_model_/icml/small/hard_seg/bc64_gate_tmax25 \
-    --log-dir ./log/icml/small/hard_seg/bc64_gate_tmax25
+    --save-dir ./_model_/icml/small/hard_seg/a3c_bc64_gate_tmax20 \
+    --log-dir ./log/icml/small/hard_seg/a3c_bc64_gate_tmax20
 
