b527950962bffb69b048ca8a0b5e62c4335bf0d2
Yi WU <jxwuyi@gmail.com>
2018-02-04 21:06:21 -0800

    add script

--- final_scripts/test_a3c.sh
+++ final_scripts/test_a3c.sh
@@ -2,18 +2,15 @@
 
 
 """
-Test A3C + Multi-Target + Visual-Signal + Gating + Tmax30
+Test A3C + Multi-Target + Segment Color + Gating
 """
-#modelfile='./_model_/cluster/small/a3c_onlyvis_gate/ZMQA3CTrainer_best.pkl'
-#logfile='./log/eval/cluster/small/small_a3c_onlyvis_gate'
+modelfile='./_model_/cluster/small/a3c_segcolor_gate/ZMQA3CTrainer_best.pkl'
+logfile='./log/eval/cluster/small/a3c_segcolor_gate'
 
-modelfile='./_model_/new/zmq_a3c/small/hard_color/bn_bc64_gate_new/ZMQA3CTrainer_succ.pkl'
-logfile='./log/eval/new/zmq_a3c/small/hard_color/small_bn_bc64_gate_new_succ'
-
-CUDA_VISIBLE_DEVICES=1 python3 eval.py --env-set small --house -20 --seed 0 --hardness 0.95 \
+CUDA_VISIBLE_DEVICES=7 python3 eval.py --env-set test --house -50 --seed 0 --hardness 0.95 \
   --success-measure see --multi-target --use-target-gating \
   --algo a3c --max-episode-len 100 --max-iters 1000 \
-  --segmentation-input color --depth-input  \
+  --segmentation-input color --depth-input \
   --rnn-units 256 --rnn-layers 1 --rnn-cell lstm \
   --store-history --log-dir "$logfile"  --warmstart "$modelfile" --batch-norm
 
--- final_scripts/test_a3c_nog.sh
+++ final_scripts/test_a3c_nog.sh
@@ -2,15 +2,15 @@
 
 
 """
-Test A3C + NO-Gating + Multi-Target + Visual + Tmax30
+Test A3C + Multi-Target + Segment Color + No-Gating
 """
-modelfile='./_model_/cluster/small/a3c_onlyvis_nog/ZMQA3CTrainer_best.pkl'
-logfile='./log/cluster/small/small_a3c_onlyvis_nog'
+modelfile='./_model_/cluster/a3c_seg_nog/ZMQA3CTrainer_best.pkl'
+logfile='./log/eval/cluster/a3c_seg_nog'
 
-CUDA_VISIBLE_DEVICES=1 python3 eval.py --env-set small --house -20 --seed 0 --hardness 0.95 \
+CUDA_VISIBLE_DEVICES=7 python3 eval.py --env-set test --house -50 --seed 0 --hardness 0.95 \
   --success-measure see --multi-target \
   --algo a3c --max-episode-len 100 --max-iters 1000 \
-  --segmentation-input none \
+  --segmentation-input color --depth-input \
   --rnn-units 256 --rnn-layers 1 --rnn-cell lstm \
   --store-history --log-dir "$logfile"  --warmstart "$modelfile" --batch-norm
 
