4d5e2b694cfd417f8638a7bd0dc7e133e6faec3a
Yi WU <jxwuyi@gmail.com>
2018-02-04 20:14:05 -0800

    update script

--- icml_scripts/test_ddpg.sh
+++ icml_scripts/test_ddpg.sh
@@ -3,7 +3,7 @@
 CUDA_VISIBLE_DEVICES=0,1 python3 train.py --seed 0 --env-set small --render-gpu 1 \
     --algo ddpg_joint --update-freq 2 --max-episode-len 10 \
     --house -3 --reward-type delta --success-measure see \
-    --multi-target --use-target-gating --use-action-gating \
+    --multi-target --use-target-gating --use-action-gating --include-object-target \
     --lrate 0.0001 --gamma 0.95 \
     --batch-size 5 --hardness 0.95 --replay-buffer-size 10000 \
     --weight-decay 0.00001 --critic-penalty 0.0001 --entropy-penalty 0.01 \
