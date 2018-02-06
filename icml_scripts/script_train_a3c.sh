00553222069772c00b937ac6007574c5edfc30e3
Yi WU <jxwuyi@gmail.com>
2018-02-06 15:28:44 -0800

    update script

--- icml_scripts/script_train_a3c.sh
+++ icml_scripts/script_train_a3c.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=0,1,2 python3 zmq_train.py --seed 0 --env-set small \
     --hardness 0.95 --reward-type delta --success-measure see \
     --multi-target --use-target-gating --include-object-target \
     --segmentation-input color --depth-input --resolution normal \
-    --render-gpu 0,1 --max-iters 100000 \
+    --render-gpu 0,1,2 --max-iters 100000 \
     --algo a3c --lrate 0.001 --weight-decay 0.00001 --gamma 0.95 --batch-norm \
     --entropy-penalty 0.1 --q-loss-coef 1.0 --grad-clip 1.0 \
     --rnn-units 256 --rnn-layers 1 --rnn-cell lstm \
