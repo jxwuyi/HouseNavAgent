69ecfe4ed59f4976dce2987d949e2a66be87a279
Yi WU <jxwuyi@gmail.com>
2018-02-07 13:56:39 -0800

    update script

--- icml_scripts/script_ddpg_seg_gate.sh
+++ icml_scripts/script_ddpg_seg_gate.sh
@@ -5,11 +5,11 @@ CUDA_VISIBLE_DEVICES=0,1 python3 train.py --seed 0 --env-set small --render-gpu 
     --house -20 --reward-type delta --success-measure see \
     --multi-target --use-target-gating --use-action-gating --include-object-target \
     --lrate 0.0001 --gamma 0.95 \
-    --batch-size 128 --hardness 0.95 --replay-buffer-size 200000 \
+    --batch-size 128 --hardness 0.95 --replay-buffer-size 400000 \
     --weight-decay 0.00001 --critic-penalty 0.0001 --entropy-penalty 0.01 \
     --batch-norm --no-debug \
     --noise-scheduler high --q-loss-coef 100 \
-    --segmentation-input color --depth-input --resolution normal --history-frame-len 4 \
-    --save-dir ./_model_/icml/small_object/hard_seg/ddpg_gate_hist_4 \
-    --log-dir ./log/icml/small_object/hard_seg/ddpg_gate_hist_4
+    --segmentation-input color --depth-input --resolution normal --history-frame-len 5 \
+    --save-dir ./_model_/icml/small_object/hard_seg/ddpg_gate_hist_5 \
+    --log-dir ./log/icml/small_object/hard_seg/ddpg_gate_hist_5
 
