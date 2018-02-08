0d6941d9ec524f2517a41c526d81882d008307a3
Yi WU <jxwuyi@gmail.com>
2018-02-07 23:10:11 -0800

    update

--- icml_scripts/eval_small_a3c_obj_gate_vis.sh
+++ icml_scripts/eval_small_a3c_obj_gate_vis.sh
@@ -1,26 +1,24 @@
 # script for model trained on small set with object targets
-"""
-# train
-# a3c-vis-gate
-CUDA_VISIBLE_DEVICES=2,1 python3 eval.py --seed 0 --env-set small --house -20 --hardness 0.95 \
-    --render-gpu 1 --success-measure see --multi-target \
-    --include-object-target --only-eval-room-target \
-    --algo a3c --max-episode-len 100 --max-iters 5000 \
-    --segmentation-input none --depth-input \
-    --rnn-units 256 --rnn-layers 1 --rnn-cell lstm --batch-norm \
-    --log-dir ./log/eval/small/a3c_obj_vis_gate_train \
-    --warmstart ./_model_/eval/small/a3c_obj_vis_gate/ZMQA3CTrainer_succ.pkl
-"""
-
 # test 
 # a3c-vis-gate
-CUDA_VISIBLE_DEVICES=2,1 python3 eval.py --seed 0 --env-set test --house -50 --hardness 0.95 \
-    --render-gpu 1 --success-measure see --multi-target \
+CUDA_VISIBLE_DEVICES=0,1 python3 eval.py --seed 0 --env-set test --house -50 --hardness 0.95 \
+    --render-gpu 1 --success-measure see --multi-target --use-target-gating \
     --include-object-target --only-eval-room-target \
     --algo a3c --max-episode-len 100 --max-iters 5000 \
     --segmentation-input none --depth-input \
     --rnn-units 256 --rnn-layers 1 --rnn-cell lstm --batch-norm \
     --log-dir ./log/eval/small/a3c_obj_vis_gate_test \
+    --warmstart ./_model_/eval/small/a3c_obj_vis_gate/ZMQA3CTrainer_succ.pkl
+
+# train
+# a3c-vis-gate
+CUDA_VISIBLE_DEVICES=0,1 python3 eval.py --seed 0 --env-set small --house -20 --hardness 0.95 \
+    --render-gpu 1 --success-measure see --multi-target --use-target-gating \
+    --include-object-target --only-eval-room-target \
+    --algo a3c --max-episode-len 100 --max-iters 5000 \
+    --segmentation-input none --depth-input \
+    --rnn-units 256 --rnn-layers 1 --rnn-cell lstm --batch-norm \
+    --log-dir ./log/eval/small/a3c_obj_vis_gate_train \
     --warmstart ./_model_/eval/small/a3c_obj_vis_gate/ZMQA3CTrainer_succ.pkl
 
 
