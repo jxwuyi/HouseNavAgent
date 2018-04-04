5a370678b3f93105ac78e8c9d8832d7e7fd7ac64
Yi Wu <jxwuyi@gmail.com>
2018-04-03 22:10:26 -0700

    add script

---
+++ nips_script/script_train_a3c_small.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CUDA_VISIBLE_DEVICES=0,1,2 python3 zmq_train.py --seed 0 --env-set small \
+    --n-house 20 --n-proc 90 --batch-size 64 --t-max 30 --max-episode-len 100 \
+    --hardness 0.95 --max-birthplace-steps 40 \
+    --reward-type new --success-measure see \
+    --multi-target --include-object-target \
+    --segmentation-input color --depth-input --resolution normal \
+    --render-gpu 0,1,2 --max-iters 100000 \
+    --algo a3c --lrate 0.001 --weight-decay 0.00001 --gamma 0.95 --batch-norm \
+    --entropy-penalty 0.1 --q-loss-coef 1.0 --grad-clip 1.0 \
+    --rnn-units 256 --rnn-layers 1 --rnn-cell lstm \
+    --report-rate 20 --save-rate 1000 --eval-rate 200000 \
+    --save-dir ./_model_/nips/small/birth40_seg/a3c_bc64_nog_tmax30 \
+    --log-dir ./log/nips/small/birth40_seg/a3c_bc64_gate_tmax30
