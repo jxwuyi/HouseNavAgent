7b907b7ad25c1a86b407a755aff55849c7280048
Yi Wu <jxwuyi@gmail.com>
2018-04-03 22:29:35 -0700

    update script

--- nips_script/script_train_a3c_large.sh
+++ nips_script/script_train_a3c_large.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0,1,2 python3 zmq_train.py --seed 0 --env-set small \
-    --n-house 20 --n-proc 90 --batch-size 64 --t-max 30 --max-episode-len 100 \
+CUDA_VISIBLE_DEVICES=3,4,5,6,7 python3 zmq_train.py --seed 0 --env-set train \
+    --n-house 200 --n-proc 200 --batch-size 64 --t-max 30 --max-episode-len 100 \
     --hardness 0.95 --max-birthplace-steps 40 \
     --reward-type new --success-measure see \
     --multi-target --include-object-target \
@@ -11,5 +11,5 @@ CUDA_VISIBLE_DEVICES=0,1,2 python3 zmq_train.py --seed 0 --env-set small \
     --entropy-penalty 0.1 --q-loss-coef 1.0 --grad-clip 1.0 \
     --rnn-units 256 --rnn-layers 1 --rnn-cell lstm \
     --report-rate 20 --save-rate 1000 --eval-rate 200000 \
-    --save-dir ./_model_/nips/small/birth40_seg/a3c_bc64_nog_tmax30 \
-    --log-dir ./log/nips/small/birth40_seg/a3c_bc64_nog_tmax30
+    --save-dir ./_model_/nips/large/birth40_seg/a3c_bc64_nog_tmax30 \
+    --log-dir ./log/nips/large/birth40_seg/a3c_bc64_nog_tmax30
