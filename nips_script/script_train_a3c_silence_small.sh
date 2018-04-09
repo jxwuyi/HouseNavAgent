f5118d42876bc2c10d065d515be3deadd4ad27c3
Yi WU <jxwuyi@gmail.com>
2018-04-08 21:34:42 -0700

    update script

--- nips_script/script_train_a3c_silence_small.sh
+++ nips_script/script_train_a3c_silence_small.sh
@@ -18,6 +18,6 @@ CUDA_VISIBLE_DEVICES=0,1,2 python3 zmq_train.py --job-name small \
     --algo a3c --lrate 0.001 --weight-decay 0.00001 --gamma 0.99 --batch-norm \
     --entropy-penalty 0.05 --q-loss-coef 1.0 --grad-clip 2.0 \
     --rnn-units 256 --rnn-layers 1 --rnn-cell lstm \
-    --report-rate 20 --save-rate 1000 --eval-rate 2000000 \
+    --report-rate 20 --save-rate 1000 --eval-rate 1000000 \
     --save-dir ./_model_/nips/large/birth20_sil10_seg/a3c_bc64_gate_tmax60_et005 \
     --log-dir ./log/nips/large/birth20_sil10_seg/a3c_bc64_gate_tmax60_et005
