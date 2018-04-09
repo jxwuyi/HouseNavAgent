414829d1f636b5463b3f81a608a93baad2d4ab75
Yi WU <jxwuyi@gmail.com>
2018-04-08 21:24:39 -0700

    update script

--- nips_script/script_train_a3c_silence_small.sh
+++ nips_script/script_train_a3c_silence_small.sh
@@ -11,7 +11,7 @@ CUDA_VISIBLE_DEVICES=0,1,2 python3 zmq_train.py --job-name small \
     --n-house 20 --n-proc 90 --batch-size 32 --t-max 60 --grad-batch 2 \
     --max-episode-len 80 \
     --hardness 0.95 --max-birthplace-steps 20 \
-    --reward-type new --success-measure see --silence-reward 10 \
+    --reward-type new --success-measure see --reward-silence 10 \
     --multi-target --use-target-gating --include-object-target \
     --segmentation-input color --depth-input --resolution normal \
     --render-gpu 0,1,2 --max-iters 100000 \
