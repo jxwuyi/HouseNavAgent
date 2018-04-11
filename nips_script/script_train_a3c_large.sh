72e121b7bf50a4fdcaaaa66c624afd57363dfaa6
Yi WU <jxwuyi@gmail.com>
2018-04-10 22:36:02 -0700

    update script

--- nips_script/script_train_a3c_large.sh
+++ nips_script/script_train_a3c_large.sh
@@ -10,7 +10,7 @@ CUDA_VISIBLE_DEVICES=3,4,5,6,7 python3 zmq_train.py --job-name large \
     --segmentation-input color --depth-input --resolution normal \
     --render-gpu 0,1,2 --max-iters 100000 \
     --algo a3c --lrate 0.001 --weight-decay 0.00001 --gamma 0.99 --batch-norm \
-    --entropy-penalty 0.05 --q-loss-coef 1.0 --grad-clip 2.0 \
+    --entropy-penalty 0.1 --q-loss-coef 1.0 --grad-clip 1.0 --adv-norm \
     --rnn-units 256 --rnn-layers 1 --rnn-cell lstm \
     --report-rate 20 --save-rate 1000 --eval-rate 1000000 \
     --save-dir ./_model_/nips/large/birth15_seg/a3c_bc128_gate_tmax60 \
--- nips_script/script_train_a3c_small.sh
+++ nips_script/script_train_a3c_small.sh
@@ -9,7 +9,7 @@ CUDA_VISIBLE_DEVICES=0,1,2 python3 zmq_train.py --seed 0 --env-set small \
     --segmentation-input color --depth-input --resolution normal \
     --render-gpu 0,1,2 --max-iters 100000 \
     --algo a3c --lrate 0.001 --weight-decay 0.00001 --gamma 0.99 --batch-norm \
-    --entropy-penalty 0.05 --q-loss-coef 1.0 --grad-clip 2.0 \
+    --entropy-penalty 0.1 --q-loss-coef 1.0 --grad-clip 1.0 --adv-norm \
     --rnn-units 256 --rnn-layers 1 --rnn-cell lstm \
     --report-rate 20 --save-rate 1000 --eval-rate 1000000 \
     --save-dir ./_model_/nips/small/birth15_seg/a3c_bc64_gate_tmax60 \
