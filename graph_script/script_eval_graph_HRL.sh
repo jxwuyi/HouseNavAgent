e883262847a8b2b13ba1b400eb96d13910b3f3e8
Yi WU <jxwuyi@gmail.com>
2018-05-07 19:33:33 -0700

    update script

--- graph_script/script_eval_graph_HRL.sh
+++ graph_script/script_eval_graph_HRL.sh
@@ -3,16 +3,16 @@
 #MODEL_DIR='./_model_/nips/large/birth15_seg_mask_curr/ZMQA3CTrainer_succ.pkl'
 MODEL_DIR='./_model_/nips_new/sup_large_no_outdoor/seg_gate_bc_curr/ZMQA3CTrainer.pkl'
 
-python3 HRL/eval_HRL.py --seed 0 --env-set test --house -50 \
+python3 HRL/eval_HRL.py --seed 0 --env-set test --house -5 \
     --hardness 0.95 --render-gpu 1 --no-outdoor-target \
     --planner graph --planner-file ./_graph_/fake/mle_fake_graph_params.pkl --n-exp-steps 50 \
     --success-measure see --multi-target --use-target-gating \
     --include-object-target --only-eval-object-target \
-    --motion rnn --max-episode-len 300 --max-iters 5000 \
+    --motion random --max-episode-len 300 --max-iters 5000 \
     --segmentation-input color --depth-input --no-outdoor-target \
     --rnn-units 256 --rnn-layers 1 --rnn-cell lstm --batch-norm \
-    --log-dir ./log/HRL/eval/mle_fake_graph \
-    --warmstart $MODEL_DIR
+    --log-dir ./log/HRL/eval/p_graph_m_pure_random
+    #--warmstart $MODEL_DIR
 
 # --max-birthplace-steps 15
 # --only-eval-object-target
--- graph_script/script_eval_random_motion.sh
+++ graph_script/script_eval_random_motion.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-python3 HRL/eval_motion.py --task-name roomnav --env-set test --house -50 --seed 0 \
+python3 HRL/eval_motion.py --task-name roomnav --env-set test \
+    --house -50 --seed 0 \
     --render-gpu 0 --hardness 0.95 \
     --segmentation-input none --depth-input \
     --success-measure see --multi-target --include-object-target \
-    --motion random --max-episode-len 100 --max-iters 5000 --store-history \
-    --log-dir ./log/graph/eval/pure_fake
+    --motion random \
+    --max-episode-len 100 --max-iters 5000 \
+    --only-eval-object-target \
+    --store-history \
+    --log-dir ./log/graph/eval/pure_random
 
 # --max-birthplace-steps 15
 # --only-eval-object-target
