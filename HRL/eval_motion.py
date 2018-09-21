aced2c46d2758cf746e0b1c7bd58af1fe08d92b1
jxwuyi <jxwuyi@gmail.com>
2018-09-21 03:15:12 -0700

    update

--- HRL/eval_motion.py
+++ HRL/eval_motion.py
@@ -22,6 +22,11 @@ def create_motion(args, task, oracle_func=None):
                 trainer_args = json.load(f)
         else:
             trainer_args = args
+        common.process_observation_shape('rnn', trainer_args['resolution_level'],
+                                         segmentation_input=trainer_args['segment_input'],
+                                         depth_input=trainer_args['depth_input'],
+                                         history_frame_len=1,
+                                         target_mask_input=trainer_args['target_mask_input'])
         import zmq_train
         trainer = zmq_train.create_zmq_trainer('a3c', 'rnn', trainer_args)
         model_file = args['warmstart']
@@ -72,11 +77,11 @@ def evaluate(args):
     common.debugger = utils.FakeLogger()
 
     # ensure observation shape
-    common.process_observation_shape('rnn', trainer_args['resolution_level'],
-                                         segmentation_input=trainer_args['segment_input'],
-                                         depth_input=trainer_args['depth_input'],
+    common.process_observation_shape('rnn', args['resolution_level'],
+                                         segmentation_input=args['segment_input'],
+                                         depth_input=args['depth_input'],
                                          history_frame_len=1,
-                                         target_mask_input=trainer_args['target_mask_input'])
+                                         target_mask_input=args['target_mask_input'])
 
     fixed_target = args['fixed_target']
     if (fixed_target is not None) and (fixed_target != 'any-room') and (fixed_target != 'any-object'):
